cb741fe3641528ec5b77f876c44509d9d436a84e
Vasanth9119 <vasanthrajkumar056@gmail.com>
2026-03-17 13:40:34 +0530

    uc4: Greeting with default name

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+
+        }
     }
 }
