67beb2479d31d0b7e53d6fc5163bf8cebab50681
Vasanth9119 <vasanthrajkumar056@gmail.com>
2026-03-17 13:06:19 +0530

    uc3 was implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,9 @@
-public class HelloApp 
-{
-    public static void main(String[] args) 
-    {
-        System.out.println("Hello World");
+public class HelloApp {
+    public static void main(String[] args) {
+        // Check if a name was passed
+        String name = (args.length > 0) ? args[0] : "World";
+        
+        // Display greeting
+        System.out.println("Hello " + name);
     }
 }
