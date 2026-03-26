1530673bfb4576380cce5fb3aefdc80f4feeb641
Vasanth9119 <vasanthrajkumar056@gmail.com>
2026-03-26 10:17:35 +0530

    uc5 was implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,10 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Check if arguments are provided
         if (args.length > 0) {
-            // Enhanced for loop to greet each argument
             for (String name : args) {
                 System.out.println("Hello, " + name + "!");
             }
-        } else {
-            // Default greeting when no arguments are given
+        } else {  
             System.out.println("Hello, World!");
         }
     }
